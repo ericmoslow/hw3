0c3a4342d25ae8b2f9ba2db3f6f04b6f71bb5522
eric moslow <eric.moslow@kellogg.northwestern.edu>
2025-05-17 03:48:11 +0000

    added lisbon, london, meat cone, further built entries infrastructure with no connectivity

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,20 @@
 class EntriesController < ApplicationController
+
+def index
+  @entries = Entry.all
+end
+
+
+
+
+
+def create
+  @entry = Entry.new
+  @entry["title"] = params["title"]
+  @entry["description"] = params["description"]
+  @entry["occurred_on"] = params["occurred_on"]
+  @entry.save
+  redirect_to "/entries"
+end
+
 end
